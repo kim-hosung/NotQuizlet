c1210e08b39c9b25ebc10fd760bffedd9e4c2e83
kim-hosung <100704957+kim-hosung@users.noreply.github.com>
2023-01-21 17:08:30 -0500

    k

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-alert("test")
+document.getElementById("myButton").onclick = function(){
+
+    var term1 = document.getElementById("term1").value;
+    var def1 = document.getElementById("def1").value;
+
+    console.log(term1, def1)
+
+
+}
