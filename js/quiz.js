31a56aa65586ade3329e0c52b73435b6166ff909
kim-hosung <100704957+kim-hosung@users.noreply.github.com>
2023-04-04 10:26:09 -0400

    l

--- js/quiz.js
+++
@@ -1,5 +0,0 @@
-let terms = localStorage.getItem("globalTerms");
-let definitions = localStorage.getItem("globalDefinitions");
-let arrayTerms = JSON.parse(terms);
-let arrayDefinitions = JSON.parse(definitions);
-
