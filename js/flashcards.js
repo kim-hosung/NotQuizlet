f5de8f72148d7d0391d0aa9520bc8a77ccee0b66
kim-hosung <100704957+kim-hosung@users.noreply.github.com>
2023-04-03 22:09:33 -0400

    working

--- js/flashcards.js
+++ js/flashcards.js
@@ -2,5 +2,58 @@ let terms = localStorage.getItem("globalTerms");
 let definitions = localStorage.getItem("globalDefinitions");
 let arrayTerms = JSON.parse(terms);
 let arrayDefinitions = JSON.parse(definitions);
+let length = arrayTerms.length;
+
+const element = document.getElementById("content");
+
+var cardNumber = 0;
+var showTerm = true;
 
 
+element.innerHTML = arrayTerms[cardNumber];
+
+
+document.getElementById("previous").onclick = function(){
+    previousCard()
+}
+
+document.getElementById("flip").onclick = function(){
+    flipCard()
+}
+
+document.getElementById("next").onclick = function(){
+    nextCard()
+}
+
+function nextCard() {
+    if(cardNumber<length-1){
+        cardNumber = cardNumber + 1;
+        element.innerHTML = arrayTerms[cardNumber];
+    }
+    else{
+        alert("You have reached the end!");
+    }
+
+    showTerm = true;
+}
+
+function previousCard() {
+    if(cardNumber != 0){
+        cardNumber = cardNumber - 1;
+        element.innerHTML = arrayTerms[cardNumber];
+    }
+    else {
+        alert("There is no previous card!");
+    }
+}
+
+function flipCard() {
+    if(showTerm){
+        element.innerHTML = arrayDefinitions[cardNumber];
+        showTerm = false;
+    }
+    else{
+        element.innerHTML = arrayTerms[cardNumber];
+        showTerm = true;
+    }
+}
